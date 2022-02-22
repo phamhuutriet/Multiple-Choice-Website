c733fd1e3b9c29989fb7248596d3200ef8ae275a
Little Panda <>
2022-02-21 21:32:14 -0700

    Implement basic CRUD for DECK

--- src/main/java/Multiple/Choice/multiplechoice/controllers/DeckController.java
+++ src/main/java/Multiple/Choice/multiplechoice/controllers/DeckController.java
@@ -1,9 +1,13 @@
 package Multiple.Choice.multiplechoice.controllers;
 
 import Multiple.Choice.multiplechoice.models.Deck;
+import Multiple.Choice.multiplechoice.models.Question;
 import Multiple.Choice.multiplechoice.service.DeckService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.rmi.server.ExportException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/decks")
@@ -28,6 +32,32 @@ public class DeckController {
         return ResponseEntity.ok(deckService.fetchDeckById(intId));
     }
 
+    @GetMapping("/{deckId}/questions")
+    public ResponseEntity<List<Question>> fetchAllQuestionByDeck(@PathVariable("deckId") String deckId) throws Exception {
+        int intDeckId = Integer.parseInt(deckId);
+        return ResponseEntity.ok(deckService.fetchAllQuestion(intDeckId));
+    }
+
+
+    @PostMapping("/{deckId}/questions")
+    public ResponseEntity<Deck> addQuestion(@PathVariable("deckId") String deckId, @RequestBody Question newQuestion) throws Exception{
+        int intDeckId = Integer.parseInt(deckId);
+        return ResponseEntity.ok(deckService.addQuestion(newQuestion, intDeckId));
+    }
+
+    @PatchMapping("/{deckId}/questions/remove/{questionId}")
+    public ResponseEntity<Deck> removeQuestion(@PathVariable("deckId") String deckId, @PathVariable("questionId") String questionId) throws Exception{
+        int intDeckId = Integer.parseInt(deckId);
+        int intQuestionId = Integer.parseInt(questionId);
+        return ResponseEntity.ok(deckService.removeQuestion(intDeckId, intQuestionId));
+    }
+
+    @PatchMapping("/{deckId}/questions/{questionId}")
+    public ResponseEntity<Deck> updateQuestion(@PathVariable("deckId") String deckId, @PathVariable("questionId") String questionId, @RequestBody Question updateQuestion) throws Exception {
+        int intDeckId = Integer.parseInt(deckId);
+        int intQuestionId = Integer.parseInt(questionId);
+        return ResponseEntity.ok(deckService.updateQuestion(updateQuestion, intDeckId, intQuestionId));
+    }
 
     // TEST METHODS
 }
--- src/main/java/Multiple/Choice/multiplechoice/service/DeckService.java
+++ src/main/java/Multiple/Choice/multiplechoice/service/DeckService.java
@@ -1,9 +1,11 @@
 package Multiple.Choice.multiplechoice.service;
 
 import Multiple.Choice.multiplechoice.models.Deck;
+import Multiple.Choice.multiplechoice.models.Question;
 import Multiple.Choice.multiplechoice.repositories.DeckRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,13 +30,44 @@ public class DeckService {
         return optionalDeck.get();
     }
 
-    public Deck updateDeckById(Deck updatedDeck, int id) throws Exception {
+    public Deck addQuestion(Question newQuestion, int id) throws Exception {
         Optional<Deck> optionalDeck = deckRepo.findById(id);
         if (optionalDeck.isEmpty()) throw new Exception("Deck's id not found");
         Deck deck = optionalDeck.get();
-        deck.setQuestions(updatedDeck.getQuestions());
+        deck.getQuestions().add(newQuestion);
 
         return deckRepo.save(deck);
+    }
+
+    public Deck removeQuestion(int deckId, int questionId) throws Exception {
+        Optional<Deck> optionalDeck = deckRepo.findById(deckId);
+        if (optionalDeck.isEmpty()) throw new Exception("Deck's id not found");
+        Deck deck = optionalDeck.get();
+        deck.getQuestions().removeIf(question -> question.getId() == questionId);
+
+        return deckRepo.save(deck);
+    }
+
+    public Deck updateQuestion(Question updatedQuestion, int deckId, int questionId) throws Exception {
+        Optional<Deck> optionalDeck = deckRepo.findById(deckId);
+        if (optionalDeck.isEmpty()) throw new Exception("Deck's id not found");
+        Deck deck = optionalDeck.get();
+
+        for (Question question: deck.getQuestions()) {
+            if (question.getId() == questionId) {
+                question.setDescription(updatedQuestion.getDescription());
+                question.setChoices(updatedQuestion.getChoices());
+            }
+        }
+
+        return deckRepo.save(deck);
+    }
+
+    public List<Question> fetchAllQuestion(int id) throws Exception {
+        Optional<Deck> optionalDeck = deckRepo.findById(id);
+        if (optionalDeck.isEmpty()) throw new Exception("Deck's id not found");
+        Deck deck = optionalDeck.get();
+        return deck.getQuestions();
     }
 
     // TEST METHODS
